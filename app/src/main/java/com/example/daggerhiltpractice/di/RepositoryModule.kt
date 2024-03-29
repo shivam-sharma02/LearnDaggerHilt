cb7836f3933ff2a9371c37bfd317f72ed631e017
Shivam Sharma <shivamsharma2577@gmail.com>
2024-03-29 18:20:56 +0530

    added different rather better method for binding interfaces or abstract classes

--- app/src/main/java/com/example/daggerhiltpractice/di/RepositoryModule.kt
+++ app/src/main/java/com/example/daggerhiltpractice/di/RepositoryModule.kt
@@ -1,4 +1,21 @@
 package com.example.daggerhiltpractice.di
 
+import com.example.daggerhiltpractice.data.repository.MyRepositoryImpl
+import com.example.daggerhiltpractice.domain.repository.MyRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindMyRepository(
+        myRepositoryImpl: MyRepositoryImpl
+    ): MyRepository
 }
