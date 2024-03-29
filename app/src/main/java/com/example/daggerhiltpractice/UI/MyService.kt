14b04873c4dab6ca0d4c1dc68fbead94742ac113
Shivam Sharma <shivamsharma2577@gmail.com>
2024-03-29 18:29:28 +0530

    added how to do it for service class which is field injection

--- app/src/main/java/com/example/daggerhiltpractice/UI/MyService.kt
+++ app/src/main/java/com/example/daggerhiltpractice/UI/MyService.kt
@@ -1,4 +1,24 @@
 package com.example.daggerhiltpractice.UI
 
-class MyService:  {
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.example.daggerhiltpractice.domain.repository.MyRepository
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MyService: Service()  {
+
+    @Inject
+    lateinit var repository: MyRepository
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
 }
--- app/src/main/java/com/example/daggerhiltpractice/UI/MyViewModel.kt
+++ app/src/main/java/com/example/daggerhiltpractice/UI/MyViewModel.kt
@@ -2,11 +2,13 @@ package com.example.daggerhiltpractice.UI
 
 import androidx.lifecycle.ViewModel
 import com.example.daggerhiltpractice.data.repository.MyRepositoryImpl
+import dagger.Lazy
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 
 @HiltViewModel
 class MyViewModel @Inject constructor(
-    private val myrepository: MyRepositoryImpl
-): ViewModel()
+    private val myrepository: Lazy<MyRepositoryImpl>
+): ViewModel() {
+}
